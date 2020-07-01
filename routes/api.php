ba13e7abba8eebf00d6fa0a4286b5b78895759d8
GilbertoParedes <gilbertowallsz06@gmail.com>
2020-07-01 00:41:53 +0000

    create endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('signup', 'Api\AuthController@signup');
+    
+    
+    // Routes protected for middleware api
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'Api\AuthController@logout');
+        Route::get('user', 'Api\AuthController@user');
+        // get products
+        Route::get('produtcs', 'Api\ProdutcController@index');
+    });
 });
